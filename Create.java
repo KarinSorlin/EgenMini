6b80d755506ae348515a851e7a1fd068af30569c
Karin Sörlin <sorlin.karin@gmail.com>
2018-12-06 10:59:12 +0100

    M

--- Create.java
+++ Create.java
@@ -1,3 +1,14 @@
 public class Create {
 
+  public static User createUser(String username, String password){
+
+    for(User user : Users.userList){
+      if(user.getUsername().equals(username));
+        return null;
+    }
+
+    User user = new User(username, password);
+    User.userList.add(user);
+    return user;
+  }
 }
